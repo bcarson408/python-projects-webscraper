b8668da9960950bdcc5a02695e67068ec6fb5c6a
bcarson408 <bcarson408@gmail.com>
2019-06-04 15:37:32 -0700

    Add files via upload
    jkdslkjs

---
+++ basic_scrapper.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+import requests
+import lxml
+import pdb;pdb.set_trace()
+
+players_dict = {}
+players=[]
+
+
+def returnSoup(url):
+	response = requests.get(url)
+	page = response.content
+	return BeautifulSoup(page,'lxml')
+
+def table_rows(table):
+	team_dict = {}
+	thisWeek_dict ={}
+	week_of_season = ""
+	for row in table[0].findAll('tr'):
+		line=[]
+		for cell in row.findAll('td'):
+			line.append(cell.text)
+		print(line)
+
+
+def main():
+  base_url ="https://www.cbssports.com/fantasy/football/stats/posvsdef/"
+  my_soup = returnSoup(base_url)
+  mytext = my_soup.get_text(strip=True)
+  doc_table = my_soup.findAll("table")
+  table_rows(doc_table)
+
+main()
